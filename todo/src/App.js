00ff68066618de5728390e4f0435a88903f707c3
mabrark <muhammad.khan@triosstudent.com>
2025-08-12 11:28:16 -0400

    Added Visibility control

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,23 +1,128 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import TodoBanner from './TodoBanner';
+import TodoCreator from './TodoCreator';
+import TodoRow from './TodoRow';
+import VisibilityControl from './VisibilityControl';
 
 function App() {
+
+  const [userName] = useState("Abrar"); // change 1
+
+  const [todoItems, setTodoItems] = useState([{action: "Buy Flowers", done: false},
+    {action: "Get Shoes", done: false},
+    {action: "Collect Tickets", done: true},
+    {action: "Call Joe", done: false}
+  ]);
+
+  const [showCompleted, setShowCompleted] = useState(true);
+
+  // const [newItemText, setNewItemText] = useState(""); // change 2
+
+  // const changeStateData = () => {
+  //   setUserName((prevName) => (prevName === "Adam" ? "Bob" : "Adam")); // change 3
+  // };
+
+  // const updateNewTextValue = (event) => { // change 4
+  //   setNewItemText(event.target.value);
+  // };
+
+  const createNewTodo = (task) => {
+    if (!todoItems
+      .find((item) => item.action === task))    
+    {
+      const updatedTodos = [...todoItems, {action: task, done: false}];
+      setTodoItems(updatedTodos);
+      localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    }
+  };
+
+  const toggleTodo = (todo) => {
+    const updatedTodos = todoItems.map((item) =>    
+      item.action === todo.action
+        ? { ...item, done: !item.done }
+        : item
+    );
+    setTodoItems(updatedTodos);
+      localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  };
+  
+  const todoTableRows = (doneValue) => todoItems.filter(item => item.done === doneValue).map(item =>
+    <TodoRow key={ item.action } item={ item } toggle={ toggleTodo } />
+  )
+
+  useEffect(() => {
+    try {
+      const data = localStorage.getItem("todos");
+      if(data)
+      {
+        const parsedData = JSON.parse(data);
+        if(Array.isArray(parsedData)) {
+          setTodoItems(parsedData);
+        }
+      }
+      else
+      {
+        [userName] = "Adam";
+        [todoItems] = [{action: "Buy Flowers", done: false},
+          {action: "Get Shoes", done: false},
+          {action: "Collect Tickets", done: true},
+          {action: "Call Joe", done: false}
+        ];
+        [showCompleted] = true;
+      }
+    }
+    catch(error) {
+      console.error("Failed to load todos:", error);
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoBanner userName={userName} todoItems={todoItems} />
+
+      <div class="m-3">
+        <TodoCreator callback={createNewTodo} />
+      </div>
+
+      <div class="container-fluid">
+        
+
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th style={{width: "75%"}}>Description</th>
+              <th style={{width: "25%"}}>Done</th>
+            </tr>
+          </thead>
+          <tbody>
+            { todoTableRows(false) }
+          </tbody>
+        </table>
+
+        <div className="bg-secondary text-white text-center p-2">
+          <VisibilityControl
+            description="Completed Tasks"
+            isChecked={showCompleted}
+            callback={(checked) => setShowCompleted(checked)} />
+        </div>
+
+        { showCompleted &&
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th style={{width: "75%"}} >Description</th>
+              <th style={{width: "25%"}}>Done</th>
+            </tr>
+          </thead>
+          <tbody>
+            { todoTableRows(true) }
+          </tbody>
+        </table>
+        }
+        
+      </div>
     </div>
   );
 }
